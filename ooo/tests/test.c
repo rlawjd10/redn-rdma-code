24a909a3be9ab53f818ab5ff2ccab1b4f3f4d411
jeongeun Kim <rlawjd10eun@gmail.com>
2025-02-18 17:56:34 +0900

    Update test.c

--- ooo/tests/test.c
+++ ooo/tests/test.c
@@ -30,7 +30,6 @@
 #define MAX(x, y) (((x) > (y)) ? (x) : (y))
 #define MIN(x, y) (((x) < (y)) ? (x) : (y))
 
-
 #define IS_ALIGNED(x, a) (((x) & ((__typeof__(x))(a) - 1)) == 0)
 
 #ifdef __cplusplus
@@ -60,14 +59,65 @@ enum sock_type {
 	SOCK_WORKER         // server에서 rdma 연산 수행 (확인필요)
 };
 
+#define SHM_PATH "/ifbw_shm"
+#define SHM_F_SIZE 128
+
+#define LAT 1
+
+// 공유 메모리 설정 
+void* create_shm(int *fd, int *res) {
+	void * addr;
+	*fd = shm_open(SHM_PATH, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
+	if (*fd < 0) {
+		exit(-1);
+	}
+
+	*res = ftruncate(*fd, SHM_F_SIZE);  // 공유 메모리 객체 크기 설정
+	if (*res < 0)
+	{
+		exit(-1);
+	}
+
+    // 공유 메모리 객체를 메모리에 매핑, NULL: 커널이 알아서 주소 지정 
+	addr = mmap(NULL, SHM_F_SIZE, PROT_WRITE, MAP_SHARED, *fd, 0);
+	if (addr == MAP_FAILED){
+		exit(-1);
+	}
+
+	return addr;
+}
+
+void destroy_shm(void *addr) {
+	int ret, fd;
+	ret = munmap(addr, SHM_F_SIZE);
+	if (ret < 0)
+	{
+		exit(-1);
+	}
+
+	fd = shm_unlink(SHM_PATH);  // 공유 메모리 객체 제거 
+	if (fd < 0) {
+		exit(-1);
+	}
+}
+
+volatile sig_atomic_t stop = 0;
+
+struct mr_context regions[MR_COUNT];
+
+int batch_size = 1;	//default - batching disabled
+int sge_count = 1;	//default - 1 scatter/gather element
+int use_cas = 0;	//default - compare_and_swap disabled
+
+int psync = 0;		// used for process synchronization
+
+char *portno = "12345";
+char *intf = "enp3s0f0";
+
 int master_sock = 0;
 int client_sock = 2;
 int worker_sock = 3;
 
-struct mr_context regions[MR_COUNT];
-
-char *portno = "12345";
-char *intf = "enp3s0f0";
 
 pthread_spinlock_t sock_lock;
 
@@ -76,7 +126,7 @@ typedef uintptr_t addr_t;
 
 uint64_t a[3] = {0, 1, 2};  
 
-
+/* --- allocate memory ---*/
 void *allocate_physical_memory(size_t size) {
     void *addr;
 
@@ -114,19 +164,85 @@ void free_physical_memory(void *addr, size_t size) {
     }
 }
 
+/* --- Returns new argc --- */
+static int adjust_args(int i, char *argv[], int argc, unsigned del)
+{
+   if (i >= 0) {
+      for (int j = i + del; j < argc; j++, i++)
+         argv[i] = argv[j];
+      argv[i] = NULL;
+      return argc - del;
+   }
+   return argc;
+}
+
+int process_opt_args(int argc, char *argv[])
+{
+   int dash_d = -1;
+
+restart:
+   for (int i = 0; i < argc; i++) {
+      //printf("argv[%d] = %s\n", i, argv[i]);
+      if (strncmp("-b", argv[i], 2) == 0) {
+         batch_size = atoi(argv[i+1]);
+         dash_d = i;
+	 argc = adjust_args(dash_d, argv, argc, 2);
+	 goto restart;
+      }
+      else if (strncmp("-e", argv[i], 2) == 0) {
+	 sge_count = atoi(argv[i+1]);
+         dash_d = i;
+	 argc = adjust_args(dash_d, argv, argc, 2);
+	 goto restart;
+      }
+      else if (strncmp("-p", argv[i], 2) == 0) {
+	 portno = argv[i+1];
+         dash_d = i;
+	 argc = adjust_args(dash_d, argv, argc, 2);
+	 goto restart;
+      }
+      else if (strncmp("-i", argv[i], 2) == 0) {
+	 intf = argv[i+1];
+         dash_d = i;
+	 argc = adjust_args(dash_d, argv, argc, 2);
+	 goto restart;
+      } 
+      else if (strncmp("-s", argv[i], 2) == 0) {
+	 psync = 1;
+	 dash_d = i;
+	 argc = adjust_args(dash_d, argv, argc, 1);
+	 goto restart;
+      } 
+      else if (strncmp("-cas", argv[i], 4) == 0) {
+	 use_cas = 1;
+	 dash_d = i;
+	 argc = adjust_args(dash_d, argv, argc, 1);
+	 goto restart;
+      } 
+   }
+
+   return argc;
+}
 
 
 int main(int argc, char **argv) {
+
+    int shm_fd, shm_ret;
     
-    int iters = atoi(argv[2]);
     char *server_ip = argv[1];
+
+    int *shm_proc = (int*)create_shm(&shm_fd, &shm_ret);
 
     pthread_spin_init(&sock_lock, PTHREAD_PROCESS_PRIVATE);
 
-    // 공유메모리 관련 코드 작성해야 됨
-    // 인자 관리 코드 작성해야 됨됨
+    argc = process_opt_args(argc, argv);
 
-    // check parameter
+    if(psync) {
+		printf("Setting shm_proc to zero\n");
+		*shm_proc = 0;
+		return 0;
+	}
+
     if (argc != 1 && argc != 3) {
         fprintf(stderr, "usage: %s <peer-address> <iters> [-p <portno>] [-e <sge count>] [-b <batch size>]  (note: run without args to use as server)\n", argv[0]);
         return 1;
@@ -143,8 +259,9 @@ int main(int argc, char **argv) {
      // rdma connection (server & client)
     init_rdma_agent(portno, regions, MR_COUNT, 2, NULL, NULL, NULL);
 
-
     if (isClient) {
+        int iters = atoi(argv[2]);
+
         if (iters > OFFLOAD_COUNT) {
             return 1;
         }
@@ -152,9 +269,7 @@ int main(int argc, char **argv) {
         // sockfd를 반환
         master_sock = add_connection(server_ip, portno, SOCK_MASTER, 1, 0);
     } 
-
-    if (!isClient) {
-
+    else {
         sleep(10);
        
         printf("---- Initializing array A ----\n");
@@ -162,11 +277,10 @@ int main(int argc, char **argv) {
             a[i] = i + 1000;
         }
 
-    } 
+    }
 
 
-    // rc_ready
-
-    // mode 마다 처리 다르게 하기
+    pthread_join(comm_thread, NULL);
+    printf("Agent thread terminated. Exiting main.\n");
 
 }
